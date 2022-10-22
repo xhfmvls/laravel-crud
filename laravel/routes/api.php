11a947701ddc94914f544a55fed4011be923acdf
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-23 02:52:53 +0700

    feat: implement api versioning and routing

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\V1\CustomerController;
+use App\Http\Controllers\API\V1\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\API\V1'], function() {
+    Route::apiResource('customers', CustomerController::class); 
+    Route::apiResource('invoices', InvoiceController::class); 
+}); 
